ae8516911a6818248bd56557a3b1d280f21e2c4d
Jim Administrator <jamesdavidconner@gamil.com>
2021-04-26 17:48:09 -0500

    reading authors to cards on DOM

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react';
 import firebase from 'firebase';
-import firebaseConfig from '../helpers/data/apiKeys';
-import './App.scss';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Card,
+  CardTitle,
+  CardText
+} from 'reactstrap';
 import AuthorForm from '../components/forms/authorForm';
+import firebaseConfig from '../helpers/data/apiKeys';
 import { getAuthors } from '../helpers/data/AuthorData';
+import './App.scss';
 
 firebase.initializeApp(firebaseConfig);
 
@@ -19,6 +25,14 @@ function App() {
   return (
   <div className='App'>
     <AuthorForm formTitle='Author Form'/>
+    <hr/>
+    {authors.map((authorInfo) => (
+      <Card body key={authorInfo.firebaseKey}>
+          <CardTitle tag="h5">{authorInfo.first_name} {authorInfo.last_name}</CardTitle>
+          <CardText>{authorInfo.email}</CardText>
+          <Button>Button</Button>
+      </Card>
+    ))}
   </div>
   );
 }
