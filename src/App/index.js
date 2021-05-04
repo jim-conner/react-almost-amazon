3816be33668c3108d0a2ef03181098adbbeea19a
Jim Administrator <jamesdavidconner@gamil.com>
2021-05-03 19:03:32 -0500

    compiling now but still can't sign in

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import NavBar from '../components/NavBar';
 import { getAuthors } from '../helpers/data/AuthorData';
 import Routes from '../helpers/Routes';
@@ -7,14 +9,32 @@ import './App.scss';
 
 function App() {
   const [authors, setAuthors] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     getAuthors().then(setAuthors);
   }, []);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@gmail.com')[0]
+        };
+        setUser(userInfoObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <>
     <Router>
-      <NavBar/>
+      <NavBar use={user}/>
       <Routes
         authors={authors}
         setAuthors={setAuthors}
